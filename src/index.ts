c14c51930d18e9921806cbd28b7d72a54b1f3cd6
SeyoonKim <Shiny@218.38.137.28>
2020-03-12 23:53:05 +0900

    API and Schema Test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('works!');
+import app from './app';
+import { Options } from 'graphql-yoga';
+
+const PORT : number | string = process.env.PORT || 4000;
+const PLAYGROUND : string = '/playground';
+const GRAPHQL_ENDPOINT : string = '/graphql';
+
+const appOptions : Options = {
+    port: PORT,
+    playground: PLAYGROUND,
+    endpoint: GRAPHQL_ENDPOINT
+}
+
+const handleAppStart = () => console.log(`Listening on port ${PORT}`);
+
+app.start(appOptions, handleAppStart);
