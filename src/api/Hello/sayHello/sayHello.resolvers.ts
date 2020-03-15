b32526f190ecb73045723581cf4d4fcbcccb2126
seyoonkim0131 <rlatpdbs@gmail.com>
2020-03-15 20:18:46 +0900

    TypeORM

--- src/api/Hello/sayHello/sayHello.resolvers.ts
+++ src/api/Hello/sayHello/sayHello.resolvers.ts
@@ -1,11 +1,11 @@
-import {Greeting} from "../../../types/graph";
+import { sayHelloResponse, SayHelloQueryArgs } from "../../../types/graph";
 
 const resolvers = {
     Query: {
-        sayHello: () : Greeting => {
+        sayHello: (_, args: SayHelloQueryArgs) : sayHelloResponse => {
             return {
                 error: false,
-                text: "love you"
+                text: `Hello ${args.name}`
             }
         }
     }
---
+++ src/ormConfig.ts
@@ -0,0 +1,16 @@
+import {ConnectionOptions} from "typeorm"
+
+const connectionOptions : ConnectionOptions = {
+    type: "postgres",
+    database: "de48qfdmn5saie",
+    synchronize: true,
+    logging: true,
+    entities: [ "entities/**/*.*" ],
+    host: process.env.DB_ENDPOIN,
+    port: 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    ssl: true
+}
+
+export default connectionOptions;
