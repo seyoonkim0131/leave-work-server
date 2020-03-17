e5af8ca24b86f3425336831821edd1a8837f6f5b
seyoonkim0131 <rlatpdbs@gmail.com>
2020-03-17 22:25:47 +0900

    bug fix

--- src/utils/privateResolver.ts
+++ src/utils/privateResolver.ts
@@ -1,5 +1,5 @@
 const privateResolver = resolverFunction => async(parent, args, context, info) => {
-    if(context.req.user) {
+    if(!context.req.user) {
         throw new Error("No JWT. I refuse to proceed.");
     }
     const resolved = await resolverFunction(parent, args, context, info);
